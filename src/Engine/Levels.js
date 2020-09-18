75eb9da45951e501b0a21dad771c131e1cb9aa4a
Filipe Fenrich <foolemdev@gmail.com>
2020-09-17 23:34:59 -0300

    More levels, release

--- src/Engine/Levels.js
+++ src/Engine/Levels.js
@@ -1,6 +1,13 @@
 import trafficRed from '../Assets/traffic-red.png';
 import trafficYellow from '../Assets/traffic-yellow.png';
 import trafficGreen from '../Assets/traffic-green.png';
+import dontPark from '../Assets/dontPark.jpeg';
+import turnLeft from '../Assets/turnLeft.png';
+import dontTurnLeft from '../Assets/dontTurnLeft.png';
+import lombada from '../Assets/lombada.png';
+import park from '../Assets/park.png';
+import pistaEscorregadia from '../Assets/pista-escorregadia.jpg';
+import stop from '../Assets/stop.jpg';
 
 export default [
   {
@@ -54,6 +61,132 @@ export default [
       {
         title: 'Reduzir',
         isCorrect: true,
+      },
+    ],
+  },
+  {
+    title: 'O que a placa significa?',
+    image: dontPark,
+    questions: [
+      {
+        title: 'Lombada',
+        isCorrect: false,
+      },
+      {
+        title: 'Vire a esquerda',
+        isCorrect: false,
+      },
+      {
+        title: 'Não estacione',
+        isCorrect: true,
+      },
+    ],
+  },
+  {
+    title: 'O que a placa significa?',
+    image: turnLeft,
+    questions: [
+      {
+        title: 'Vire a direita',
+        isCorrect: false,
+      },
+      {
+        title: 'Vire a esquerda',
+        isCorrect: true,
+      },
+      {
+        title: 'Estacione',
+        isCorrect: false,
+      },
+    ],
+  },
+  {
+    title: 'O que a placa significa?',
+    image: lombada,
+    questions: [
+      {
+        title: 'Lombada',
+        isCorrect: true,
+      },
+      {
+        title: 'Vire a esquerda',
+        isCorrect: false,
+      },
+      {
+        title: 'Estacione',
+        isCorrect: false,
+      },
+    ],
+  },
+  {
+    title: 'O que a placa significa?',
+    image: park,
+    questions: [
+      {
+        title: 'Permitido estacionar',
+        isCorrect: true,
+      },
+      {
+        title: 'Dê a preferência',
+        isCorrect: false,
+      },
+      {
+        title: 'Não estacione',
+        isCorrect: false,
+      },
+    ],
+  },
+  {
+    title: 'O que a placa significa?',
+    image: pistaEscorregadia,
+    questions: [
+      {
+        title: 'Semáforo a frente',
+        isCorrect: false,
+      },
+      {
+        title: 'Pare',
+        isCorrect: false,
+      },
+      {
+        title: 'Pista escorregadia',
+        isCorrect: true,
+      },
+    ],
+  },
+  {
+    title: 'O que a placa significa?',
+    image: stop,
+    questions: [
+      {
+        title: 'Pare',
+        isCorrect: true,
+      },
+      {
+        title: 'Vire a esquerda',
+        isCorrect: false,
+      },
+      {
+        title: 'Lombada',
+        isCorrect: false,
+      },
+    ],
+  },
+  {
+    title: 'O que a placa significa?',
+    image: dontTurnLeft,
+    questions: [
+      {
+        title: 'Pista escorregadia',
+        isCorrect: false,
+      },
+      {
+        title: 'Proibido virar a esquerda',
+        isCorrect: true,
+      },
+      {
+        title: 'Vire a esquerda',
+        isCorrect: false,
       },
     ],
   },
